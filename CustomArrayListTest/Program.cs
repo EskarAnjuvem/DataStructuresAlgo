1c98529160950b09146e05c94f0ed74e645257f3
Sanjeev Kumar Mahato <eskar.anjuvem@gmail.com>
2024-09-02 10:47:31 +0530

    Implementing Array List

---
+++ CustomArrayListTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using List;
+
+namespace CustomArrayListTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomArrayList<string> shoppingList = new CustomArrayList<string>();
+            shoppingList.Add("Milk");
+            shoppingList.Add("Honey");
+            shoppingList.Add("Olives");
+            shoppingList.Add("Dragon Fruit");
+            shoppingList.Add("Lettuce");
+
+            shoppingList.DisplayList();
+            Console.WriteLine("The shopping list is {0} items long", shoppingList.Count);
+
+            string item1 = shoppingList.RemoveAt(2);
+            Console.WriteLine("Item removed: {0}", item1);
+
+            shoppingList.Insert("Black Pepper", 2);
+            shoppingList.DisplayList();
+            Console.WriteLine("The shopping list is {0} items long", shoppingList.Count);
+
+            Console.WriteLine("Dragon Fruit is at index {0}", shoppingList.IndexOf("Dragon Fruit")); 
+
+            Console.ReadLine();
+        }
+    }
+}
--- List/Program.cs
+++ List/Program.cs
@@ -2,14 +2,122 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace List
 {
-    internal class Program
+    public class CustomArrayList<T>
     {
-        static void Main(string[] args)
+        private T[] arr;
+        public const int INITIAL_CAPACITY = 4;
+        private int count;
+        public int Count
         {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        public CustomArrayList(int capacity = INITIAL_CAPACITY)
+        {
+            this.arr = new T[capacity];
+            this.count = 0;
+        }
+
+        public void Add(T item)
+        {
+            GrowIfArrIsFull();
+            this.arr[count] = item;
+            this.count++;
+        }
+
+        public void Insert(T item, int index)
+        {
+            if (index > this.count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Invalid Index: " + index);
+            }
+            GrowIfArrIsFull();
+            Array.Copy(this.arr, index, this.arr, index + 1, this.count - index);
+            this.arr[index] = item;
+            this.count++;
+        }
+
+        public T RemoveAt(int index)
+        {
+            if( index >= this.count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Invalid Index : " + index);
+            }
+            T item = this.arr[index];
+
+            Array.Copy(this.arr, index + 1, this.arr, index, this.count - index - 1);
+            this.arr[this.count - 1] = default(T);
+            this.count--;
+            return item;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < this.arr.Length; i++)
+            {
+                if (Object.Equals(this.arr[i], item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Contains(T item)
+        {
+            bool result = ((IndexOf(item)) != -1);
+            return result;
+        }
+        
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= this.count || index > 0)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid index " + index);
+                }
+                return this.arr[index];
+            }
+            set 
+            {
+                if (index >= this.count || index > 0)
+                {
+                    throw new ArgumentOutOfRangeException("Invalid index " + index);
+                }
+                this.arr[index] = value;
+            }
+        }
+
+        public void DisplayList()
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                Console.WriteLine(" -> "+ arr[i]);
+            }
+        }
+        private void GrowIfArrIsFull()
+        {
+            if ((this.count + 1) > (this.arr.Length))
+            {
+                T[] newArray = new T[this.arr.Length * 2];
+                Array.Copy(this.arr, newArray, this.count);
+                this.arr = newArray;
+            }
+        }
+
+        public void Clear()
+        {
+            this.arr = new T[INITIAL_CAPACITY];
+            this.count = 0;
         }
     }
 }
